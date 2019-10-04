c9d1c328ff04a2e2d35f6246a7b1bf9b5e374f45
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-10-04 02:19:24 +0200

    fixes

--- js/compiler.js
+++ js/compiler.js
@@ -13,31 +13,30 @@
 let fs = require('fs');
 let path = require('path');
 
-// files in order
+// JavaScript files in order
 let files = [
     'utility.js',
     'main.js'
 ];
 
-// read files in order and combine their content into a string
-let total_code = '';
+// combine content of the JavaScript files into a string
+let total_content = '';
 files.forEach(file => {
     
     // add space between files
-    if (total_code != '') {
-        total_code += "\n\n\n\n\n\n";
+    if (total_content != '') {
+        total_content += "\n\n\n\n\n\n";
     }
     
-    // read file and add content to total code
+    // read file and add content to string
     let file_path = path.join(__dirname, 'source', file);
-    total_code += fs.readFileSync(file_path, 'utf-8');
+    total_content += fs.readFileSync(file_path, 'utf-8');
     
 });
-total_code = '"use strict";\n\n' + total_code;
+total_content = '"use strict";\n\n' + total_content;
 
-
-// minify total code using Uglify component
-let minified = require('uglify-js').minify(total_code, {
+// minify total code using Uglify
+let minified = require('uglify-js').minify(total_content, {
     compress : {},
     mangle : {},
     output : {
@@ -48,7 +47,7 @@ let minified = require('uglify-js').minify(total_code, {
 
 // if Uglify failed, don't create a minified file
 if (typeof(minified.code) === 'undefined') {
-    console.error('ERROR: Minified code equals "undefined". Uglify.js probably failed.\nAre there any ES6 components or errors in the source code?');
+    console.error('ERROR: Minified code equals "undefined". Uglify.js probably failed.\nAre there any errors or ES6 components in the JavaScript source code?');
 }
 else {
     // create minified file
@@ -56,6 +55,6 @@ else {
     fs.writeFileSync(filepath, minified.code, 'utf8');
 }
 
-// also create non-minified file
+// also create compiled, but non-minified file
 let filepath = path.join(__dirname, 'app.js');
-fs.writeFileSync(filepath, total_code, 'utf8');
+fs.writeFileSync(filepath, total_content, 'utf8');
